b1c40de6d1e4290385404993708d113cf88475a8
limJaemin99 <limjaemin99@gmail.com>
2023-10-16 23:57:48 +0900

    Index 파일 생성
    ● Index [1차] 생성
    - md 파일 변환 라이브러리 사용하여 README.md 적용하기
    - CSS 수정 및 보완하기
    - footer 고민
    - gif 보완하기

---
+++ src/main/java/org/teamplus/mvc/controller/test.java
@@ -0,0 +1,13 @@
+package org.teamplus.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class test {
+
+    @GetMapping("/index")
+    public void index(){
+
+    }
+}
