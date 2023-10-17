9c9d087b979033ad5ed7e112c6c71e0b0d88311c
KingoftheSloth <131847767+KingoftheSloth@users.noreply.github.com>
2023-10-17 13:31:11 +0900

    TEST) HTML에 md파일 표시하기
    Test 클래스안에
    README.md 문법 확인을 위해 내용변경
    경로탐색을 위해
    resources/manuals 로 이동
    Markdown 강조 문법 중 취소선은 적용 안됨
    
    md파일 html 전환 코드 삽입
    
    ContextPath 경로 찾기 오류로 인해
    Dashboard controller 코드 변경
    
    md 파일 읽어오기 라이브러리 dependency 추가
    commonmark

--- src/main/java/org/teamplus/mvc/controller/test.java
+++ src/main/java/org/teamplus/mvc/controller/test.java
@@ -1,13 +1,49 @@
 package org.teamplus.mvc.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+@Slf4j
 @Controller
 public class test {
+        private final static String LOCAL_MANUAL_PATH = "manuals/";
 
-    @GetMapping("/index")
-    public void index(){
+        @GetMapping("/")
+        public String markdownView(@RequestParam(required = false) String page, Model model) throws Exception {
+            log.info("page: {}", page);
+            if (page == null)
+                page="README.md";
+            String markdownValueFormLocal = getMarkdownValueFormLocal(page);
+            Parser parser = Parser.builder().build();
+            Node document = parser.parse(markdownValueFormLocal);
+            HtmlRenderer renderer = HtmlRenderer.builder().build();
 
+            model.addAttribute("contents", renderer.render(document));
+
+
+
+            return "index"; // index.html을 반환합니다.
+        }
+
+        public String getMarkdownValueFormLocal(String manualPage) throws Exception {
+            StringBuilder stringBuilder = new StringBuilder();
+            ClassPathResource classPathResource = new ClassPathResource(LOCAL_MANUAL_PATH + manualPage);
+
+            BufferedReader br = Files.newBufferedReader(Paths.get(classPathResource.getURI()));
+            br.lines().forEach(line -> stringBuilder.append(line).append("\n"));
+
+            return stringBuilder.toString();
+        }
     }
-}
+
+
